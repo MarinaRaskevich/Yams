933b21b3db95252c4c201602c979258c8836e9d1
JACOB Frederic <136260435+fredjacobbb@users.noreply.github.com>
2024-04-17 16:10:00 +0200

    Add files via upload

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,1 +1,70 @@
-//Fonctions
+let firstThrow = () => {
+    for(i = 1; i < 6; i++){
+        listDes.push(Math.floor(Math.random() * 6) + 1)
+    }
+    showValues();
+}
+
+let secondThrow = () => {
+    listDes = [];
+    let numberOfnone = 5 - containerThrow.querySelectorAll(".d-none").length;
+    console.log(numberOfnone);
+    for (let i = 0; i < numberOfnone; ++i) {
+        listDes.push(Math.floor(Math.random() * 6) + 1);
+    }
+    containerThrow.innerHTML = "";
+    showValues();
+    console.log(domRelatedDes);
+}
+
+let showValues = () => {
+
+    listDes.forEach((de,j) => {
+        let p = document.createElement("p");
+        p.setAttribute("data-value", de)
+        p.innerText = de;
+        //AJOUT DU Dé DANS LE DOM
+        containerThrow.appendChild(p);
+
+        p.addEventListener("click", (e) => {
+            console.log(e.target.getAttribute("data-value"));
+            e.target.classList.add("d-none");
+            domRelatedDes.push(e.target.innerText);
+        })
+
+    });
+}
+
+let selectedDe = () => {
+
+}
+
+let full = () => {
+
+}
+
+let littleSuite = () => {
+
+}
+
+let suite = () => {
+
+}
+
+let chance = () => {
+
+}
+
+let square = () => {
+    
+}
+
+let brelan = () => {
+
+}
+
+let yams = () => {
+
+}
+
+firstThrow();
--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,1 +1,15 @@
-//Sélecteurs et écouteurs d'événements
+let listDes = [];
+
+let throwDice = [];
+
+let domRelatedDes = []
+
+let containerThrow = document.getElementsByClassName("container-throw")[0];
+
+let throwButton = document.getElementById("throw");
+
+
+throwButton.addEventListener('click', () => {
+    secondThrow();
+})
+
