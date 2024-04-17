e0424594d73cc37df2e7117b93c3f48acd6376da
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-04-17 12:15:17 +0200

    Fonctions pour les points

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,36 +1,79 @@
-let throwDes = () => {
-    for(i = 1; i < 6; i++){
-        listDes.push(Math.floor(Math.random() * 6) + 1)
+let firstThrow = () => {
+  for (i = 1; i < 6; i++) {
+    listDes.push(Math.floor(Math.random() * 6) + 1);
+  }
+  console.log(listDes);
+};
+
+//Ajouter des chifres identiques dans une sélection aléatoire de dés
+const sumDuplicates = (value, array) => {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == value) {
+      count++;
     }
-    console.log(listDes);
-}
+  }
+  return count * value;
+};
 
-let full = () => {
+// Vérification des valeurs pour yams
+const yamsScore = (array) => {
+  if (array.every((number, i, arr) => number == arr[0])) {
+    return 50;
+  } else {
+    return 0;
+  }
+};
 
-}
+// Calcul de la valeur pour Chance
+const chanceScore = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
 
-let littleSuite = () => {
+firstThrow();
+console.log(chanceScore(listDes));
 
-}
-
-let suite = () => {
-
-}
-
-let chance = () => {
-
-}
-
-let square = () => {
-    
-}
-
-let brelan = () => {
-
-}
-
-let yams = () => {
-
-}
-
-throwDes();
+const calculatePoints = (operation, distValues) => {
+  let total;
+  switch (operation) {
+    case "total1":
+      total = sumDuplicates(1, distValues);
+      break;
+    case "total2":
+      total = sumDuplicates(2, distValues);
+      break;
+    case "total3":
+      total = sumDuplicates(3, distValues);
+      break;
+    case "total4":
+      total = sumDuplicates(4, distValues);
+      break;
+    case "total5":
+      total = sumDuplicates(5, distValues);
+      break;
+    case "total6":
+      total = sumDuplicates(6, distValues);
+      break;
+    case "brelan":
+      break;
+    case "square":
+      break;
+    case "full":
+      break;
+    case "smallSuite":
+      break;
+    case "bigSuite":
+      break;
+    case "yams":
+      total = yamsScore(distValues);
+      break;
+    case "chance":
+      total = chanceScore(distValues);
+      break;
+  }
+  return total;
+};
